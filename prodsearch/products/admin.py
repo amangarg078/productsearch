5fc9d904b6ee110455b891cd3f668e36cddde2f1
Aman Garg <amangarg078@gmail.com>
2021-09-24 00:41:06 +0530

    add admin view

--- prodsearch/products/admin.py
+++ prodsearch/products/admin.py
@@ -7,7 +7,10 @@ from .models import Product, ProductImage, ProductCategory
 class ProductImageAdmin(admin.ModelAdmin):
     list_display = ['product', 'position']
 
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['display_name', 'name']
+
 
 admin.site.register(Product)
 admin.site.register(ProductImage, ProductImageAdmin)
-admin.site.register(ProductCategory)
+admin.site.register(ProductCategory, ProductCategoryAdmin)
